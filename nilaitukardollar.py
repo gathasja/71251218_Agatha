2f0920b9e325ba56b26efc2c7c14f4cb7a343288
gatha <gathasja@gmail.com>
2026-02-22 10:14:21 +0700

    71251218

--- nilaitukardollar.py
+++
@@ -1,9 +0,0 @@
-kursusd = 13950
-
-print('Program konversi US$ ke IDR')
-print('Kurs saat ini 1 US$ = ', kursusd, 'Rupiah')
-
-jumlahusd = float(input('Masukkan jumlah uang yang mau ditukar ke Rupiah: '))
-dalamrupiah = jumlahusd*kursusd
-
-print('Hasil konversi = Rp. ', dalamrupiah)
